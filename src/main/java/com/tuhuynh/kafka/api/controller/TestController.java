5d43b0dec41ed0021e22bba1165dccdef6752f7e
Tu Huynh <huynhminhtufu@gmail.com>
2021-06-01 02:57:03 +0700

    update path

--- src/main/java/com/tuhuynh/kafka/api/controller/TestController.java
+++ src/main/java/com/tuhuynh/kafka/api/controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
-    @GetMapping("/")
+    @GetMapping
     public String test() {
         return "Hello World";
     }
